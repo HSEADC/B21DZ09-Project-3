cba7fd5c987d84b8fed4f9583bf9fcc6ddb53af0
dima <69328790+dimaast@users.noreply.github.com>
2023-03-25 08:45:00 +0300

    big changes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,59 @@
 import './index.css'
+
+function removeFifthCard() {
+  const element = document.getElementsByClassName("Five");
+  element.remove();
+}
+
+removeFifthCard()
+//
+// function removeFifthCard() {
+//   const element = document.getElementById('Five')
+//   element.ClassList.remove('Five')
+//   document.body.appendChild(element)
+// }
+//
+const cardAnswers = [
+  {
+    question: "Куда засыпать порошок  в стиральной машине?",
+    answer: "В правый отсек — для предварительной стирки, в левый — для основной стирки, а в средний — кондиционер"
+  },
+  {
+    question: "Как перекрыть воду?",
+    answer: "Тут будет ответ"
+  },
+  {
+    question: "Как зажечь газовую плиту?",
+    answer: "Тут будет ответ"
+  },
+  {
+    question: "Как отпереть сломанный замок?",
+    answer: "Тут будет ответ"
+  },
+  {
+    question: "Номера экстренных служб",
+    answer: "Тут будет ответ"
+  }
+]
+
+function showCardAnswer(cardAnswers) {
+  const element = document.createElement('div')
+  element.innerText = cardAnswers
+  document.body.appendChild(element)
+}
+
+// setTimeout(() => {
+//   showCardAnswer(cardAnswers[0])
+//   }, 2000)
+
+// setTimeout(
+//   () => { console.log(cardAnswers[0]) },
+//   2000)
+
+document.addEventListener('DOMContentLoaded'), () => {
+  showCardAnswer(cardAnswers[0])
+}
+
+// setTimeout(() => {
+//   showCardAnswer(cardAnswers[0])
+// }, 2000)
